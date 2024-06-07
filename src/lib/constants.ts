999bebcf005f0de6752a02393a04b7efd70664e4
Grayson Gantek <graysonleegantek@gmail.com>
2024-06-07 08:03:06 -0400

    add constants for social media links

---
+++ src/lib/constants.ts
@@ -0,0 +1,3 @@
+export const FACEBOOK_URL = 'https://www.facebook.com/VacationsWithCharacter/'
+export const INSTAGRAM_URL = 'https://www.instagram.com/vacationswithcharacter/'
+export const PINTEREST_URL = 'https://www.pinterest.com/where_fun_bgins/'
