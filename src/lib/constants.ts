76a1218e2f3cba2824f3488c6fc06a24f65b7df0
Grayson Gantek <graysonleegantek@gmail.com>
2024-10-06 12:50:30 -0400

    Add external blog URL to constants file

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,3 +5,4 @@ export const QUOTE_URL = 'https://fs29.formsite.com/ST9lsf/form7/index.html';
 export const AFFILIATE_URL_DISNEY =
   'https://www.disneytravelcenter.com/ms36150a45/';
 export const AFFILIATE_URL_SANDALS = 'https://www.sandals.com/?referral=144559';
+export const BLOG_URL = 'https://vacationswithcharacter.blog/';
