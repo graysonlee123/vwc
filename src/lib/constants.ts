2498cddef4365c6f63216c0db68d4323acc1b9df
Grayson Gantek <graysonleegantek@gmail.com>
2024-10-06 12:45:19 -0400

    Add constants for external URLs
    Add constants for affiliate URLs and quote form

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,7 @@
 export const FACEBOOK_USERNAME = 'VacationsWithCharacter';
 export const INSTAGRAM_USERNAME = 'vacationswithcharacter';
 export const PINTEREST_USERNAME = 'where_fun_bgins';
+export const QUOTE_URL = 'https://fs29.formsite.com/ST9lsf/form7/index.html';
+export const AFFILIATE_URL_DISNEY =
+  'https://www.disneytravelcenter.com/ms36150a45/';
+export const AFFILIATE_URL_SANDALS = 'https://www.sandals.com/?referral=144559';
