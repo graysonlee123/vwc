cefa00d955d13d8d8aeaa8d360057a2eef770381
Grayson Gantek <github@graysn.com>
2024-10-09 20:44:38 -0400

    Setup Astro config file
    Configures site URL, redirects, and disabled dev toolbar.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,20 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://www.vacationswithcharacter.com',
+  redirects: {
+    '/about-us.html': '/about/',
+    '/all-inclusive-resorts.html': '/',
+    '/disney.html': '/',
+    '/happy-clients.html': '/',
+    '/our-services.html': '/',
+    '/privacy-policy.html': '/privacy-policy',
+    '/terms-and-conditions.html': '/terms-and-conditions',
+    '/tickets.html': '/',
+    '/universal-orlando.html': '/',
+  },
+  devToolbar: {
+    enabled: false,
+  },
+});
