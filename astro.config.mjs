716ed12f9624b2a5051eb810f52af1ffe2295b39
Grayson Gantek <github@graysn.com>
2024-10-14 20:18:38 -0400

    Add and configure Astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,4 +18,5 @@ export default defineConfig({
   devToolbar: {
     enabled: false,
   },
+  integrations: [sitemap()],
 });
