26e56cc9d4c543a04f915d186baa8e2a99d3588a
Grayson Gantek <github@graysn.com>
2024-10-22 18:04:11 -0400

    Moves redirects into new  configuration file
    Also adds  file for ignoring the new Vercel configuraton file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.vacationswithcharacter.com',
-  redirects: {
-    '/about-us.html': '/about/',
-    '/all-inclusive-resorts.html': '/',
-    '/disney.html': '/',
-    '/happy-clients.html': '/',
-    '/our-services.html': '/',
-    '/privacy-policy.html': '/privacy-policy',
-    '/terms-and-conditions.html': '/terms-and-conditions',
-    '/tickets.html': '/',
-    '/universal-orlando.html': '/',
-  },
   devToolbar: {
     enabled: false,
   },
