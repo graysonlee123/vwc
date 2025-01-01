5b45c0cd4f1f976d1d5422444128b43fd5b7e9c4
Grayson Gantek <github@graysn.com>
2025-01-01 01:16:33 -0500

    Update Astro config to use dynamic site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+const site = (() => {
+  if (process.env.VERCEL_PROJECT_PRODUCTION_URL) {
+    console.log({VERCEL_PROJECT_PRODUCTION_URL: process.env.VERCEL_PROJECT_PRODUCTION_URL})
+    return 'https://' + process.env.VERCEL_PROJECT_PRODUCTION_URL
+  }
+
+  return 'http://localhost:4321'
+})()
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.vacationswithcharacter.com',
+  site,
   devToolbar: {
     enabled: false,
   },
