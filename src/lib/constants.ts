ca26e3c49bc1c8069a0535651a8045083cfa00fe
Grayson Gantek <github@graysn.com>
2025-02-11 21:24:51 -0500

    Remove affiliate links for Sandals/Beaches in favor of Get a Quote link

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,6 +4,4 @@ export const PINTEREST_USERNAME = 'where_fun_bgins';
 export const QUOTE_URL = 'https://fs29.formsite.com/ST9lsf/form7/index.html';
 export const AFFILIATE_URL_DISNEY =
   'https://www.disneytravelcenter.com/ms36150a45/';
-export const AFFILIATE_URL_SANDALS = 'https://www.sandals.com/?referral=144559';
-export const AFFILIATE_URL_BEACHES = 'https://www.beaches.com/?referral=144559';
 export const BLOG_URL = 'https://vacationswithcharacter.blog/';
